ad4954e91b8b9b43c4be4384f2732c3e71e7359b
DipaliTalware <d.talware@gmail.com>
2024-08-30 10:49:04 +0200

    Bonus Task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 //Step 5: Perform element-wise addition of both arrays and store the result in a third array. Print the result array to the console.
@@ -13,6 +14,7 @@ public class Main {
         task2(numbers);
         task3(numbers);
         task5(numbers, numbers2);
+        readText();
     }
 
     //Step 1: Create an array with numbers from 1 to 10 and print it to the console.
@@ -31,7 +33,7 @@ public class Main {
         System.out.println(acc);
     }
 
-    public static void task3(int[] numbers){
+    public static void task3(int[] numbers) {
         System.out.println("Task 3");
         int largestNumber = numbers[0];
         for (int element : numbers) {
@@ -42,7 +44,7 @@ public class Main {
         System.out.println(largestNumber);
     }
 
-    public static void task5(int[] numbers, int[]numbers2) {
+    public static void task5(int[] numbers, int[] numbers2) {
         int minLength = Math.min(numbers.length, numbers2.length);
         int[] sumArray = new int[minLength];
 
@@ -52,5 +54,29 @@ public class Main {
 
         System.out.println("Task 5");
         System.out.println(Arrays.toString(sumArray));
+    }
+
+    //  Bonus Task:  Implement a program that reads a text from the console and then outputs the letters individually. Tip: Learn about the Scanner class.
+    public static void readText() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a text to read: ");
+
+        String enteredText = scanner.nextLine();
+        System.out.println("Bonus Task");
+
+
+//        with for each loop
+        for (char c : enteredText.toCharArray()) {
+            System.out.println(c);
+        }
+
+
+//        with for loop
+        for (int i = 0; i < enteredText.length(); i++) {
+            System.out.println(enteredText.charAt(i));
+
+        }
+
+        System.out.println("The text is: " + enteredText);
     }
 }
